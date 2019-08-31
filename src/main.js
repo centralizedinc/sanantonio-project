249df53c053ead3ddcc6ab946cac2f5ec32e97fa
abalita <ariel.a.balita@gmail.com>
2019-09-01 05:37:48 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,9 +6,21 @@ import './registerServiceWorker'
 import './plugins/ant-design-vue.js'
 
 import axios from 'axios';
+import * as VueGoogleMaps from 'vue2-google-maps'
+import VueGeolocation from 'vue-browser-geolocation';
 
 Vue.config.productionTip = false
 Vue.prototype.$http=axios
+
+Vue.use(VueGeolocation)
+Vue.use(VueGoogleMaps, {
+  load: {
+    // key: 'AIzaSyCHB6d8PoTLPZ2dtnxSPSHFpeu3pxkiA6s' || process.env.VUE_APP_GOOGLE_MAP_KEY,
+    key:  process.env.VUE_APP_GOOGLE_MAP_KEY
+  },
+  installComponents: true
+})
+
 
 new Vue({
   router,
