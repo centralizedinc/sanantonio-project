acd99c52e1819d7d15d10934848503582036e113
VPBelo <venus.belo.rhi@gmail.com>
2019-09-17 12:33:50 +0800

    chat widget

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -126,8 +126,24 @@ export default {
         }
     }
 }
-</script>
+window.fbAsyncInit = function() {
+        FB.init({
+          appId: "314648529334246",
+          xfbml: true,
+          version: "v4.0"
+        });
+      };
 
+      (function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk");
+</script>
 <style>
 
 </style>
