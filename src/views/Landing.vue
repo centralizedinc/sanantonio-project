3113efbcf0e07a38ec4e968f202844636618419d
VPBelo <venus.belo.rhi@gmail.com>
2019-09-19 19:15:11 +0800

    widget

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -155,6 +155,15 @@
         />
       </a-col>
     </a-row>
+    <div id="fb-root"></div>
+    <div
+      class="fb-customerchat"
+      attribution="setup_tool"
+      page_id="109048890468083"
+      theme_color="#0084ff"
+      logged_in_greeting="Hi! I'm Mayor Alcala, How can I help you?"
+      logged_out_greeting="Hi! I'm Mayor Alcala, How can I help you?"
+    ></div>
   </div>
 </template>
 
@@ -184,23 +193,37 @@ export default {
     }
   }
 };
-window.fbAsyncInit = function() {
-  FB.init({
-    appId: "314648529334246",
-    xfbml: true,
-    version: "v4.0"
-  });
-};
-
-(function(d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
 </script>
+<!-- Load Facebook SDK for JavaScript -->
+<script>
+      (function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk");
+    </script>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId: "314648529334246",
+          xfbml: true,
+          version: "v4.0"
+        });
+      };
+
+      (function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk");
+    </script>
 <style>
 </style>
