f9bbb9db7cf7335e2a9df6f9f61f6bed2ad42bc1
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-09-19 23:36:02 +0800

    remove fb jdk

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -207,27 +207,6 @@ export default {
     }
   }
 };
-/**
- *  <script>
-window.fbAsyncInit = function() {
-  FB.init({
-    appId: "394573761441193",
-    xfbml: true,
-    version: "v4.0"
-  });
-};
-
-(function(d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
-</script>
- */
 </script>
 <!-- Load Facebook SDK for JavaScript -->
    
